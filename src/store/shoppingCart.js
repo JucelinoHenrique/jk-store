d2c711fbf914b3ed513770fe60d95036603334d9
Oscar Silva <oscar.reis2011@hotmail.com>
2022-05-01 12:49:42 -0300

    criado reducer e funcionalidade para adicionar produtos ao carrinho

---
+++ src/store/shoppingCart.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  products: [],
+  totalQuantity: 0,
+  totalPrice: 0,
+}
+
+export const shoppingCart = createSlice({
+  name: "shoppingCart",
+  initialState,
+  reducers: {
+    addProduct: (state, action) => {
+      state.products.push(action.payload);        
+      state.totalQuantity += 1;
+      state.totalPrice = state.products.reduce((acc, product) => {
+        return acc + product.price;
+      }, 0);
+    },
+    removeProduct: (state, action) => {
+      state.products.splice(action.payload, 1);     
+      state.totalQuantity -= 1;
+      state.totalPrice = state.products.reduce((acc, product) => {
+        return acc + product.price;
+      }, 0);             
+    },
+  }
+})
+
+export const { addProduct, removeProduct } = shoppingCart.actions;
+
+export default shoppingCart.reducer;
