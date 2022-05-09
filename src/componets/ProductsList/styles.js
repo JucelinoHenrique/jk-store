b96722feb64b66d14aa65bc307a54d9e6363ddbe
JucelinoHenrique <jucelino.oliveira2002@gmail.com>
2022-05-09 09:49:34 -0300

    adcionei telas de loading para as paginas

--- src/componets/ProductsList/styles.js
+++ src/componets/ProductsList/styles.js
@@ -14,8 +14,20 @@ export const ButtonAdd = styled.button`
  width: 100px;
  background-image: linear-gradient(120deg, #ae8500 0%, #fe3d08 100%);
 
+`
 
+export const Loading = styled.div`
+animation: is-rotating 1s infinite;
+width: 50px;
+height: 50px;
+border-radius: 50%;
+border: 3px solid black;
+border-top: 3px solid aqua;
 
-
+@keyframes is-rotating {
+  to{
+    transform: rotate(1turn);
+  }
+}
 
 `
